15d43d81ac4f5a3e9bca61ab73ec5f1f70b443b8
juanmaishere <rsjuanmanuel@hotmail.com>
2024-07-04 00:16:28 -0300

    feat

--- src/components/ObjModel.jsx
+++ src/components/ObjModel.jsx
@@ -1,14 +1,12 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { useLoader, useFrame, extend } from '@react-three/fiber';
 import { OBJLoader, MTLLoader } from 'three-stdlib';
-import * as THREE from 'three';
 
 extend({ OBJLoader, MTLLoader });
 
 export default function ObjModel(props) {
   const group = useRef();
   const [rotationSpeed, setRotationSpeed] = useState(0.002);
-  const [modelDimensions, setModelDimensions] = useState({ width: 0, height: '100vh', depth: 0 });
 
   const materials = useLoader(MTLLoader, '/models2/Satellite_1236.mtl');
   const obj = useLoader(OBJLoader, '/models2/Satellite_1236.obj', (loader) => {
@@ -18,10 +16,6 @@ export default function ObjModel(props) {
 
   useEffect(() => {
     if (obj) {
-      const box = new THREE.Box3().setFromObject(obj);
-      const size = box.getSize(new THREE.Vector3());
-      setModelDimensions({ width: size.x, height: size.y, depth: size.z });
-
       group.current.add(obj);
     }
 
@@ -30,7 +24,7 @@ export default function ObjModel(props) {
     }, 3300); // Stop rotation after 4 seconds
 
     return () => clearTimeout(timer); // Clean up the timer on component unmount
-  }, [obj, setModelDimensions]);
+  }, [obj]);
 
   useFrame(() => {
     if (group.current) {
